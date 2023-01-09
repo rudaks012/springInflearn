90b89a02100904bb0485283dbc249982f326cde1
gyeongman <rudaks012@naver.com>
2023-01-09 21:35:10 +0900

    Feat : DI를 이용한 APP CONFIG 추가

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,14 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = new MemberServiceImpl(new MemoryMemberRepository());
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
+        System.out.println("findMember = " + findMember.getName());
     }
 }
