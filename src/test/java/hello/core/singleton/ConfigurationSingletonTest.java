6967a5b3764849de4f2fe683714fa2f0fdf58537
gyeongman <rudaks012@naver.com>
2023-01-24 17:30:43 +0900

    Feat : @Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -8,6 +8,7 @@ import hello.core.member.MemberService;
 import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ConfigurationSingletonTest {
@@ -29,6 +30,13 @@ public class ConfigurationSingletonTest {
 
         assertThat(memberRepository1).isSameAs(memberRepository2);
 
+    }
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
     }
 
 }
